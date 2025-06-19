52c2c823191d842759093432de52284b856d7a72
martinzeman333 <martin@datelier.cz>
2025-06-19 21:35:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/static/<path:filename>')
-def static_files(filename):
-    return send_from_directory('static', filename)
 
 @app.route('/health')
 def health():
